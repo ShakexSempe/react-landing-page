3a19ab3231ab8decd926eff749ec1ff47c7d1e50
Ray-Shakex <bshakex@gmail.com>
2021-11-18 15:04:09 +0200

    Navbar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import syntex_1 from "./assets/syntex-1.jpg";
 import syntex_3 from "./assets/syntex-3.jpg";
 import Hero from "./components/Hero";
 import Slider from "./components/Slider";
+import Navbar from "./components/Navbar";
+
+// navbar links 
+const navbarLinks = [
+  { url: "#", title: "Home"},
+  { url: "#", title: "About"},
+  { url: "#", title: "Contact"},
+]
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar navbarLinks={navbarLinks} />
       <Hero imageSrc={hero}/>
       <Slider imageSrc={syntex_1} title={"Responsive Web Design"} 
               subtitle={"We make responsive websites for devices ranging from small smartphones to large display screens"
