952e90e5881dc5faf23107c14313900785866579
Ray-Shakex <bshakex@gmail.com>
2021-11-19 10:29:58 +0200

    navbar media query

--- src/App.js
+++ src/App.js
@@ -22,8 +22,8 @@ function App() {
       <Slider imageSrc={syntex_1} title={"Responsive Web Design"} 
               subtitle={"We make responsive websites for devices ranging from small smartphones to large display screens"
               }/>
-      <Slider imageSrc={syntex_3} title={"Immersive User Interface and User Experience"} 
-              subtitle={"Syntex uses transitions and animations to create beautiful pages users enjoy"
+      <Slider imageSrc={syntex_3} title={"User Interface & User Experience"} 
+              subtitle={"Syntex uses transitions and animations to create beautiful user interface and immersive user experience"
               }
               flipped={true}
               />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Navbar.css";
 import {FiMenu, FiX} from "react-icons/fi";
 
@@ -8,10 +8,25 @@ import {FiMenu, FiX} from "react-icons/fi";
 // }
 
 const Navbar = ({navbarLinks}) => {
+    const [menuClicked, setMenuClicked] = useState(false);
+
+    const toggleMenuClick = () => {
+        setMenuClicked(!menuClicked);
+    };
+
     return (
         <nav className="navbar">
             <span className="navbar_logo">syntex</span>
-            <ul className="navbar_list"> 
+        {/* if menuClicked then show X icon else if not clicked then show menu icon */}
+            {menuClicked ? (
+                <FiX size={25} className="navbar_menu" onClick={toggleMenuClick}/>
+            
+            ) : (
+                <FiMenu size={25} className="navbar_menu" onClick={toggleMenuClick}/>
+
+                ) 
+            }
+            <ul className={!menuClicked ? "navbar_list" : "navbar_list navbar_list--active"}> 
             {navbarLinks.map((item) => {
                 return <li className="navbar_item" key={item.title}>
                     <a className="navbar_link" href={item.url}>
